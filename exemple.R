c5d2080f1ddc221cf8aa9ec24491de976a49d0ca
rCarto <timothee.giraud@cnrs.fr>
2018-03-06 16:10:27 +0100

    update

--- exemple.R
+++ exemple.R
@@ -1,4 +1,6 @@
-library("rnaturalearth")
+
+# Download raw data
+
 # download world basemap, ocean and graticules
 download.file(url = "https://raw.githubusercontent.com/riatelab/basemaps/master/World/countries.geojson",
               destfile = "data/country.geojson")
@@ -9,39 +11,50 @@ download.file(url="http://www.un.org/en/development/desa/population/migration/da
               destfile="data/mig.xlsx")
 
 
+##############################################################################
 
 
-
+# Prepare data
 
 # libraries
 library(sf)
-library(cartography)
 library(countrycode)
 library(readxl)
 
-# import
-countries <- st_read(dsn = "data/country.geojson", quiet = TRUE, stringsAsFactors = F)
+# import world countries and graticules
+countries <- st_read(dsn = "data/country.geojson", stringsAsFactors = F)
 mig <- data.frame(read_excel("data/mig.xlsx", skip = 15, sheet = "Table 16"))
 
+# import migration data
 mig$ISO3 <- countrycode(sourcevar = mig$X__4, 
                         origin = "iso3n", 
                         destination = "iso3c")
+# merge countries geometries and mig data
 countries <- merge(countries[,c("ISO3", "ISO2","name")], mig[,-(1:5)], 
                    by.x="ISO3", by.y="ISO3", all.x=TRUE)
+
+# modify col names with ISO3 codes
 coln <- countrycode(sourcevar = names(countries)[7:238], 
                     origin = "country.name",destination = "iso3c")
 names(countries)[7:238] <- coln
 names(countries)[48] <- "GGY"
 
-
+# export the layer
 st_write(countries, "data/countrriesmig.geojson")
+
+
+
+##############################################################################
 
 
 # import
 graticules <- st_read(dsn = "data/graticule.geojson", quiet = TRUE)
+countries <-  st_read(dsn = "data/countrriesmig.geojson", quiet = TRUE)
+
+# plot the layers
 par(mar = c(0,0,1.2,0))
 # affichage des différentes couches
-plot(st_geometry(graticules), col = "white", lwd = 0.2, bg="lightblue")
+plot(st_geometry(graticules), col = "lightblue", lwd = 0.2)
 plot(st_geometry(countries), col = "ivory4", border ="ivory3", 
      lwd = 0.5, add=TRUE)
 
@@ -51,44 +64,57 @@ crs <- "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
 countries <- st_transform(x = countries, crs = crs)
 graticules <- st_transform(x = graticules, crs = crs)
 
+# plot the layers
+plot(st_geometry(graticules), col = "lightblue", lwd = 0.2)
+plot(st_geometry(countries), col = "ivory4", border ="ivory3", 
+     lwd = 0.5, add=TRUE)
 
+
+library(cartography)
+# first map of syrian migrants in UE
 par(mar = c(0,0,1.2,0))
 UEcode <- c("AT", "BE", "BG", "CH", "CY", "CZ", "DE", "DK", "EE", "ES", 
             "FI", "FR", "GR", "HR", "HU", "IE", "IS", "IT", "LI", "LT", "LU", 
             "LV", "MK", "MT", "NL", "NO", "PL", "PT", "RO", "SE", "SI", "SK", 
             "GB")
-UE2015 <- countries2015[countries2015$ISO2 %in% UEcode,]
+UE2015 <- countries[countries$ISO2 %in% UEcode,]
+
 lim <- st_bbox(countries[countries$ISO3 %in% c("PRT", "FIN"),])
 plot(st_geometry(graticules), col = "white", lwd = 0.2, bg="lightblue", 
      xlim = lim[c(1,3)], ylim = lim[c(2,4)]) 
 plot(st_geometry(countries), col = "ivory4", border ="ivory3", 
      lwd = 0.5, add=TRUE)
 plot(UE2015$geometry, col="black", add=T, border="white")
-propSymbolsLayer(x = UE2015, var = "DZA",
+propSymbolsLayer(x = UE2015, var = "SYR",
                  inches=0.5, col="red",legend.frame = T, 
                  legend.pos = "topright")
-layoutLayer("Danger!", sources = "",author = "")
+layoutLayer("Syrian migrant in UE", sources = "",author = "", tabtitle = T)
 
 
-
+# 2nd map, syrian migrant in Middle East
 lim <- st_bbox(countries[countries$ISO3 %in% c("PRT", "FIN", "SAU"),])
 plot(st_geometry(graticules), col = "white", lwd = 0.2, bg="lightblue", 
      xlim = lim[c(1,3)], ylim = lim[c(2,4)]) 
 plot(st_geometry(countries), col = "ivory4", border ="ivory3", 
      lwd = 0.5, add=TRUE)
-propSymbolsLayer(x = countries2015, var = "JOR", 
+propSymbolsLayer(x = countries, var = "SYR", 
                  inches=1.2, col="red", legend.pos = "n")
 x <- c(3180000, 3072000, 3202000, 3992000, 3851000, 2693000)
 y <- c(5050000, 4465000, 3827000, 2625000, 3480000, 2765000)
-fl <- countries2015[order(countries2015$SYR, decreasing = T),
+options(scipen=3)
+fl <- countries[order(countries$SYR, decreasing = T),
                     c("ISO3","SYR")][1:6,]
-options(digits=1)
 labs <- paste0(fl$ISO3,"\n",round(fl$SYR, -3)) 
 text(x, y, labels = labs, font=4, col="white", cex=0.8)
 layoutLayer("Ouf!", sources = "",author = "")
 
 
-names(countries2015)
+
+
+
+
+
+
 
 ## small multiples
 st_precision(countries) <- 1
@@ -100,10 +126,10 @@ par(mfrow=c(6,4),mar = c(0,0,1.2,0))
 for(i in 1:24){
   plot(st_geometry(graticules), col = "lightblue",border=NA, bg="white")
   plot(st_geometry(sc), col = "ivory4", border ="ivory4",add=TRUE)
-  propSymbolsLayer(x = countries2015, var = names(countries2015)[i+6],
+  propSymbolsLayer(x = countries, var = names(countries)[i+6],
                    fixmax=1000000, 
                    inches=0.1, col="red", legend.pos = "n", lwd=0.2)
-  mtext(names(countries2015)[i+6], 3,  adj = c(0.25))
+  mtext(names(countries)[i+6], 3,  adj = c(0.25))
 }
 
 
--- exemple2.R
+++ exemple2.R
@@ -1,76 +1,291 @@
+################## Install packages #####################################
+# install packages
+# install.packages("sf")
+# install.packages("cartography")
+# install.packages("readxl")
+
+
+
+
+
+
+
+################## Import geo layer #####################################
+
+# load libraries
 library(sf)
-com <- st_read(dsn = "data/communes_reg_84.geojson", stringsAsFactors = F)
-com <- st_transform(com, 2154 )
-com <- com[,c("INSEE_COM")]
+
+# import communes of Auvergne-Rhone-Alpes region
+reg84 <- st_read(dsn = "data/communes_reg_84.geojson", stringsAsFactors = F)
+
+# view information on the geo layer com
+head(reg84)
+
+# display the layer
+plot(st_geometry(reg84))
+
+# Extract the Rhone Departement 
+dep69 <- reg84[reg84$CODE_DEPT==69,]
+plot(st_geometry(dep69))
 
 
+
+
+
+
+
+
+################## Import data #####################################
 
 library(readxl)
-csp <- read_excel("/home/tim/Téléchargements/pop-act2554-csp-cd-6814.xls", 
+# import data from an INSEE excel file 
+csp <- read_excel("data/pop-act2554-csp-cd-6814.xls", 
                   sheet = "COM_2014", skip = 15)
-depcode <-  unique(com$CODE_DEPT)
-csp <- data.frame(csp[csp$DR %in% depcode,])
-csp$INSEE_COM <- paste0(csp$DR, csp$CR)
-csp <- csp[,-c(1:5)]
-reg <- st_union(com)
+head(csp)
 
-com[substr(com$INSEE_COM, 1,4) =="6938", "INSEE_COM" ] <- "69123"
-com <- aggregate(com, by=list(com$INSEE_COM), FUN = head, 1)
-com <- com[,-1]
-com <- merge(com, csp, by="INSEE_COM", all.x=TRUE)
+# select data for Rhone
+csp69 <- data.frame(csp[csp$DR %in% 69,])
+head(csp69)
+
+# create INSEE_COM code
+csp69$INSEE_COM <- paste0(csp69$DR, csp69$CR)
+
+# suppress unused fields (the first 5 fields)
+csp69 <- csp69[,-c(1:5)]
+
+## ATTENTION ##
+dep69[substr(dep69$NOM_COM, 1,4) =="LYON",  ] 
+csp69[csp69$LIBELLE=="Lyon", ]
+
+# aggregate Lyon arrondissements in dep69 into 1 commune
+dep69[substr(dep69$NOM_COM, 1,4) =="LYON", "INSEE_COM" ] <- "69123"
+dep69 <- aggregate(dep69, by=list(dep69$INSEE_COM), FUN = head, 1)
+st_geometry(dep69) <- st_cast(dep69$geometry, "MULTIPOLYGON")
+dep69 <- dep69[,"INSEE_COM"]
+plot(st_geometry(dep69))
+
+# merge com layer and data
+dep69 <- merge(dep69, csp69, by="INSEE_COM", all.x=TRUE)
+head(dep69)
+
+# use friendly names
 cs <- c("agr1", "agr0", "art1", "art0", "cad1", "cad0", "int1","int0", 
         "emp1", "emp0","ouv1", "ouv0")
-names(com)[3:14] <- cs
+names(dep69)[3:14] <- cs
 
-st_geometry(com) <- st_cast(com$geometry, "MULTIPOLYGON")
-
-com$agr <- com$agr0 + com$agr1
-com$art <- com$art0 + com$art1
-com$cad <- com$cad0 + com$cad1
-com$int <- com$int0 + com$int1
-com$emp <- com$emp0 + com$emp1
-com$ouv <- com$ouv0 + com$ouv1
-dev.off()
-par(mfrow=c(2,3), mar=c(0,0,1.2,0))
-plot(reg, col="grey", border=NA)
-propSymbolsLayer(com, var = "agr", fixmax=60000, lwd=0.2, col="ivory1", legend.pos = "n")
-
-plot(reg, col="grey", border=NA)
-propSymbolsLayer(com, var = "art", fixmax=60000, lwd=0.2, col="ivory1", legend.pos = "n")
-
-plot(reg, col="grey", border=NA)
-propSymbolsLayer(com, var = "cad", fixmax=60000, lwd=0.2, col="ivory1", legend.pos = "n")
-
-plot(reg, col="grey", border=NA)
-propSymbolsLayer(com, var = "int", fixmax=60000, lwd=0.2, col="ivory1", legend.pos = "n")
-
-plot(reg, col="grey", border=NA)
-propSymbolsLayer(com, var = "emp", fixmax=60000, lwd=0.2, col="ivory1", legend.pos = "n")
-
-plot(reg, col="grey", border=NA)
-propSymbolsLayer(com, var = "ouv", fixmax=60000, lwd=0.2, col="ivory1", legend.pos = "n")
+dep69
+for (i in 3:14){
+  dep69[,i] <- round(dep69[,i, drop=T],0)
+}
 
 
-warnings()
+# agregate field (employed + unemployed)
+dep69$agr <- dep69$agr0 + dep69$agr1
+dep69$art <- dep69$art0 + dep69$art1
+dep69$cad <- dep69$cad0 + dep69$cad1
+dep69$int <- dep69$int0 + dep69$int1
+dep69$emp <- dep69$emp0 + dep69$emp1
+dep69$ouv <- dep69$ouv0 + dep69$ouv1
+dep69$act <- dep69$agr + dep69$art + dep69$cad + dep69$int + dep69$emp + dep69$ouv
 
-warning()
-dev.off()
-summary(com)
+head(dep69)
+
+# export in geojson format (===> Magrit)
+st_write(dep69, "output/dep69.geojson", delete_dsn = T)
 
 
 
-
-
-
-
-
-com$ouvs <- 100*com$ouv0/com$act
+################## Cartography #####################################
 library(cartography)
-plot(reg, col="grey", border=NA)
-propSymbolsChoroLayer(com[com$act>0,], var = "act", var2 = "emps")
-choroLayer(com[com$ouv1>0,], var = "ouvs", border="white", method="q6", 
-           lwd=0.2, legend.values.rnd = 2, add=T, 
-           col=carto.pal("turquoise.pal", 6))
 
-smoothLayer(x = com[com$emp1>0,], var = "ouv1", var2 = "act", span=10000, beta = 2,
-                 resolution = 20000, mask = as(reg, "Spatial"))
+
+# change projection (lambert93)
+dep69 <- st_transform(dep69, 2154)
+plot(st_geometry(dep69))
+
+
+# Map of active population
+plot(st_geometry(dep69), col="lightblue", border="ivory")
+propSymbolsLayer(dep69, var = "act", col="red")
+title("Population active")
+
+
+# Custom map of active population
+par(mar=c(0.2,0.2,1.4,0.2), bg="grey95")
+plot(st_geometry(dep69), col="lightblue", border="ivory")
+propSymbolsLayer(dep69, var = "act", col="darkblue", inches = 0.6, 
+                 border = "white", lwd=0.7, symbols = "square",
+                 legend.style = "e", legend.pos="topright",
+                 legend.title.txt = "Nombre d'actifs\n(2014)", 
+                 legend.values.rnd = -1)
+barscale(size = 10)
+north(pos = "topleft", col = "darkblue")
+layoutLayer(title = "Population active dans le département du Rhône", 
+            sources = "Insee, 2018", author = "cartography 2.0.2", 
+            col = "darkblue", coltitle = "white", 
+            frame = TRUE, scale = NULL, north = FALSE)
+
+# get ideal output ratio for the dep69 layer
+exp_dim <- getFigDim(x = dep69, width = 600, 
+                     mar = c(0.2,0.2,1.4,0.2), res = 100)
+
+png(filename = "output/pop_act.png",
+    width = exp_dim[1], height = exp_dim[2], res = 100)
+# Custom map of active population
+par(mar=c(0.2,0.2,1.4,0.2), bg="grey95")
+plot(st_geometry(dep69), col="lightblue", border="ivory")
+propSymbolsLayer(dep69, var = "act", col="darkblue", inches = 0.6, 
+                 border = "white", lwd=0.7, symbols = "square",
+                 legend.style = "e", legend.pos="topright",
+                 legend.title.txt = "Nombre d'actifs\n(2014)", 
+                 legend.values.rnd = -1)
+barscale(size = 10)
+north(pos = "topleft", col = "darkblue")
+layoutLayer(title = "Population active dans le département du Rhône", 
+            sources = "Insee, 2018", author = "cartography 2.0.2", 
+            col = "darkblue", coltitle = "white", 
+            frame = TRUE, scale = NULL, north = FALSE)
+dev.off()
+
+
+
+## Compare two maps
+# use fixmax param
+par(mar=c(0,0,1.2,0), bg="grey95", mfrow=c(1,2))
+plot(st_geometry(dep69), col="lightblue", border="ivory")
+propSymbolsLayer(dep69, var = "cad", col="darkblue", 
+                 inches = 0.4, fixmax=63991,
+                 border = "white", lwd=0.5,
+                 legend.pos="n")
+north(pos = "topleft", col = "darkblue")
+layoutLayer(title = "Cadres", 
+            sources = "Insee, 2018", author = "cartography 2.0.2", 
+            col = "darkblue", coltitle = "white", postitle = "center",
+            frame = F, scale = NULL, north = FALSE)
+plot(st_geometry(dep69), col="lightblue", border="ivory")
+propSymbolsLayer(dep69, var = "ouv", col="darkred", 
+                 inches = 0.4, fixmax=63991,
+                 border = "white", lwd=0.7, 
+                 legend.pos="n")
+barscale(size = 10)
+layoutLayer(title = "Ouvriers", 
+            sources = "", author = "", 
+            col = "darkred", coltitle = "white",  postitle = "center",
+            frame = F, scale = NULL, north = FALSE)
+# use custom legend
+legendCirclesSymbols(pos = "topright", title.txt = "Nombre d'actifs\n(2014)", 
+                     var = c(10,10000,25000,64000),
+                     inches = 0.4, col = "lightgrey")
+
+## explore a map interactively
+# install.packages(mapview)
+# library(mapview)
+# mapview(dep69) 
+
+# add a few toponymes
+labelLayer(x = dep69[dep69$INSEE_COM %in% c("69091","69243", "69256","69264"),], 
+           txt = "LIBELLE", pos=3, halo=TRUE)
+
+
+
+
+# Carte de Typologie
+par(mar=c(0,0,1.2,0), bg="grey95", mfrow=c(1,1))
+dep69$dom <- "Indéterminé"
+dep69$r <- dep69$ouv / dep69$cad
+dep69$r[is.na(dep69$r)] <- 0
+dep69[dep69$r > 1.1,"dom"] <- "Plus d'ouvriers que de cadres"
+dep69[dep69$r < 0.91,"dom"] <- "Plus de cadres que d'ouvriers"
+
+typoLayer(dep69, var="dom", 
+          legend.values.order = c("Plus d'ouvriers que de cadres", 
+                                  "Plus de cadres que d'ouvriers",
+                                  "Indéterminé" ),
+          col = c('darkred', 'darkblue', 'grey'), 
+          border = "white", lwd=0.5, legend.title.txt = "Type dominant") 
+layoutLayer(title = "Ouvriers vs. Cadres", 
+            sources = "", author = "", 
+            col = "darkred", coltitle = "white",  postitle = "center",
+            frame = F, scale = NULL, north = FALSE)      
+
+par(mar=c(0,0,1.2,0), bg="grey95", mfrow=c(1,1))
+plot(st_geometry(dep69), col="lightblue", border="ivory")
+propSymbolsTypoLayer(dep69, var = "act",var2="dom", 
+          legend.var2.values.order = c("Plus d'ouvriers que de cadres", 
+                                  "Plus de cadres que d'ouvriers",
+                                  "Indéterminé" ),
+          col = c('darkred', 'darkblue', 'grey'), inches = 0.45,
+          legend.var.title.txt = "Nb. actifs", legend.var.pos = "right",
+          border = "white", lwd=0.8, legend.var2.title.txt = "Type dominant") 
+layoutLayer(title = "Ouvriers vs. Cadres", 
+            sources = "", author = "", 
+            col = "darkred", coltitle = "white",  postitle = "center",
+            frame = F, scale = NULL, north = FALSE)
+
+
+
+
+# Cartes choroplèthes
+dep69$pcad <- 100 * dep69$cad / dep69$act
+summary(dep69$pcad)
+par(mfrow=c(1,2))
+boxplot(dep69$pcad)
+hist(dep69$pcad, 20)
+rug(dep69$pcad)
+
+bks <- getBreaks(v = dep69$pcad, method = "q6")
+cols <-carto.pal("green.pal", 3,"wine.pal",3)
+par(mar=c(0.2,0.2,1.4,0.2), bg="grey95", mfrow=c(1,1))
+choroLayer(dep69, var = "pcad", breaks = bks, 
+           col = cols, border = "grey80", 
+           lwd = 0.4, legend.pos = "topright", 
+           legend.title.txt = "Part des cadres\n(en %)")
+layoutLayer(title = "Les cadres", 
+            sources = "", author = "", theme = "green.pal", 
+            col = "darkred", coltitle = "white",  postitle = "center",
+            frame = TRUE, scale = 10)
+north(pos = "topleft", south = TRUE)
+
+
+
+
+
+grid <- getGridLayer(x = dep69, cellsize = 3000*3000, 
+                     type = "hexagonal", var = c('cad', 'act'))
+grid$pcad <- 100*grid$cad/grid$act
+par(mar=c(0.2,0.2,1.4,0.2), bg="grey95", mfrow=c(1,1))
+choroLayer(grid, var = "pcad", breaks=bks, 
+           col = cols, border = "grey80", 
+           lwd = 0.4, legend.pos = "topright", 
+           legend.title.txt = "Part des cadres\n(en %)")
+layoutLayer(title = "Les cadres", 
+            sources = "", author = "", theme = "green.pal", 
+            col = "darkred", coltitle = "white",  postitle = "center",
+            frame = TRUE, scale = 10)
+north(pos = "topleft", south = TRUE)
+
+
+grid$cad100 <- grid$cad * 100
+smoothLayer(x = grid, var = "cad100", var2 = "act", typefct = "exponential", 
+                 span = 2500, beta = 2, breaks = bks, col = cols,
+                 legend.pos = "topright", mask =dep69,
+                 legend.title.txt = "Part des cadres\n(en %)", 
+                 border = "grey90", lwd = 0.2)
+north(pos = "topleft", south = TRUE)
+layoutLayer(title = "Les cadres", 
+            sources = "", author = "", theme = "green.pal", 
+            col = "darkred", coltitle = "white",  postitle = "center",
+            frame = TRUE, scale = 10)
+text(x = 866875, y = 6531871, 
+     labels = "Lissage par potentiels\n fonction exponnentielle\n span = 2.5km, beta = 2", 
+     font = 3, cex = 0.8)
+
+
+
+
+
+
+
+
+
+
